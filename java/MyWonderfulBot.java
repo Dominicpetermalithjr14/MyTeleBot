acc18a2fdc2ce7875cd79a2c92a57bae68338727
Dominic loang peter <107580404+Dominicpetermalithjr14@users.noreply.github.com>
2023-01-21 10:50:54 +0300

    Update MyWonderfulBot.java

--- java/MyWonderfulBot.java
+++ java/MyWonderfulBot.java
@@ -1,13 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
- * @author bahiru
+ * @author Peterson
  */
-public class MyWonderfulBot {
+
+
+
+import org.telegrambots.api.methods.send.SendMessage;
+import org.telegrambots.api.objects.Update;
+import org.telegrambots.bots.TelegramLongPollingBot;
+import org.telegrambots.exceptions.TelegramApiException;
+
+public class MyWonderfulBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update){
+    if(update.hasMessage()&& update.getMessage().hasText()){
+        String message-text=update.getMessage().getText();
+        long chat-id=update.getMessage().getChatId();
+        
+        
+        SendMessage message=new SendMessage()
+                .setChatId(chat-id)
+                .setText(message=text);
+        
+        try {
+            execute(message);
+        } catch(TelegramApiException e){
+            e.printStackTree();
+        }
+    }
+    
+        @Override
+    public String getBotUsername(){
+        return "peteSon-jr";
+    }
+        @Override
+    public String getBotToken(){
+        return "BotFather";
+    }
     
 }
