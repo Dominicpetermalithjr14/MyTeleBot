c920d2be48b0fb3af98f0a747d4fb123987d0a29
Dominic loang peter <107580404+Dominicpetermalithjr14@users.noreply.github.com>
2023-01-21 10:43:24 +0300

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,4 +1,20 @@
+package com.mytel.myteleg;
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramApi;
+import org.telegram.telegram.telegrambots.exceptions.TelegramApiException;
 public class Main {
-    
+    public static void main(String[] args) {
+     //initialize Api context
+     ApiContextInitializer.init();
+     
+     TelegramBots botsApi=new TelegramBotsApi();
+     
+     
+     try{
+         botsApi.registerBot(new MyWonderfulBot());
+     }catch (TelegramApiException e) {
+         e.printStackTree();
+     }
+    }
 }
+    
